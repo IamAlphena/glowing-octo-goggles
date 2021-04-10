85fa1c81b5f72f3652336adc9b0c0b37bba6ab72
Alphena <alphena@gmail.com>
2021-04-10 11:50:39 -0700

    new project in the sandbox

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+
